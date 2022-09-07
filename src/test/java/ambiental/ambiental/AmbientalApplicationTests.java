6226e7f25d5e3073d78c21716d38efecf2e5d61c
Alexis Rincón <marincon444@misena.edu.co>
2022-09-07 09:19:41 -0500

    AMBIENTAL

---
+++ src/test/java/ambiental/ambiental/AmbientalApplicationTests.java
@@ -0,0 +1,13 @@
+package ambiental.ambiental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbientalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
