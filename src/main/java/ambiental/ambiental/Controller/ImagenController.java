0bf0754febc3c6736f4c072cdd13336f8801870e
cristhianlopez14 <cristhianl0306@gmail.com>
2022-09-19 14:40:27 -0500

    CRUD IMAGEN: registrar y eliminar

--- src/main/java/ambiental/ambiental/Controller/ImagenController.java
+++ src/main/java/ambiental/ambiental/Controller/ImagenController.java
@@ -3,10 +3,16 @@ package ambiental.ambiental.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
+import ambiental.ambiental.Model.Imagen;
 import ambiental.ambiental.Service.IImagenService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/imagen")
@@ -20,4 +26,39 @@ public class ImagenController {
         
         return "views/imagen/imagen";
     }
+
+    //Eliminar
+    @GetMapping("/delete/{idImagen}")
+    public String delete (@PathVariable Integer idImagen){
+
+        if(idImagen > 0){
+            imagenI.delete(idImagen);
+        }
+		return "redirect:../listar";
+    }
+
+     //Agregar Imagen
+     @PostMapping ("/add")
+     public String add(@Valid Imagen imagen ,BindingResult res, Model m, SessionStatus status){
+         if(res.hasErrors()){
+             return "views/imagen/registrar";
+         }
+         imagenI.save(imagen);
+         status.setComplete();
+         return "redirect:listar";
+     }
+ 
+     @GetMapping("/formulario")
+     public String formulario(Model m){
+         Imagen imagen = new Imagen();
+       
+         m.addAttribute("imagen", imagen);
+         return "views/imagen/registrar";
+     }
+
+     
+
+
+
+    
 }
